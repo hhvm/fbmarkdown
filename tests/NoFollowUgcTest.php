2c53d0d7b30d91910979707cb8c21b5f987d0c2b
Allison Tielking <atielking@slack-corp.com>
2020-09-24 14:18:05 -0700

    phase2 no follow ugc for links

---
+++ tests/NoFollowUgcTest.php
@@ -0,0 +1,88 @@
+<?hh // strict
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the MIT license found in the
+ *  LICENSE file in the root directory of this source tree.
+ *
+ */
+
+namespace Facebook\Markdown;
+
+use type Facebook\HackTest\DataProvider;
+use function Facebook\FBExpect\expect;
+use namespace HH\Lib\{Str};
+
+final class NoFollowUgcTest extends TestCase {
+    protected function assertXSSExampleMatches(
+        string $name,
+        string $in,
+        string $expected_html,
+        ?string $extension,
+    ): void {
+        $parser_ctx = (new ParserContext())
+            ->enableHTML_UNSAFE()
+            ->enableTrustedInput_UNSAFE()
+            ->disableExtensions();
+        $render_ctx = (new RenderContext())
+            ->disableExtensions()
+            ->addNoFollowUGCAllLinks();
+        if ($extension !== null) {
+            $parser_ctx->enableNamedExtension($extension);
+            $render_ctx->enableNamedExtension($extension);
+        }
+
+        $ast = parse($parser_ctx, $in);
+        $actual_html = (new HTMLRenderer($render_ctx))->render($ast);
+
+        // Improve output readability
+        $actual_html = Str\replace($actual_html, "\t", self::TAB_REPLACEMENT);
+        $expected_html = Str\replace(
+            $expected_html,
+            "\t",
+            self::TAB_REPLACEMENT,
+        );
+
+        expect($actual_html)->toBeSame(
+            $expected_html,
+            "HTML differs for %s:\n%s",
+            $name,
+            $in,
+        );
+    }
+
+    <<DataProvider('getNoFollowUgcExamples')>>
+    public function testXSSExample(string $in, string $expected_html): void {
+        $this->assertXSSExampleMatches(
+            'unnamed',
+            $in,
+            $expected_html,
+            /* extension = */ 'table',
+        );
+    }
+
+    public function getNoFollowUgcExamples(): vec<(string, string)> {
+        return vec[
+            // LINKS
+            tuple(
+                "[facebook](https://www.facebook.com)",
+                "<p><a href=\"https://www.facebook.com\" rel=\"nofollow ugc\">facebook</a></p>\n",
+            ),
+            // AUTOLINKS
+            tuple(
+                "<https://www.facebook.com>",
+                "<p><a href=\"https://www.facebook.com\" rel=\"nofollow ugc\">https://www.facebook.com</a></p>\n",
+            ),
+
+            // LINK REFERENCE DEFINITIONS
+            tuple(
+                "[foo]:
+https://www.facebook.com
+
+[foo]",
+                "<p><a href=\"https://www.facebook.com\" rel=\"nofollow ugc\">foo</a></p>\n",
+            ),
+        ];
+    }
+}
